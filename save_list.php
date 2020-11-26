fa2617f58b16c9fd9432a261a152de2c41536a3a
TheFoxStudio <contact@palerius.com>
2020-11-26 16:34:54 +0100

    changed safe_list to work with multiple users

--- save_list.php
+++ save_list.php
@@ -1,8 +1,10 @@
 <?php
-$myfile = fopen("data.xml", "w") or die("Unable to open file!");
+$myfile = fopen("userdata/".$_POST[userid]."_data.xml", "w") or die("Unable to open file!");
 
 fwrite($myfile, $_POST[xml]);
 fclose($myfile);
+
+chmod("userdata/".$_POST[userid]."_data", 0660);
 
 return true;
 ?>
